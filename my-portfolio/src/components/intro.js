e73bd2e577e30409f71e8ebbc71ade437948481e
lifeKhomola <khomolavhutshilo5@gmail.com>
2022-05-12 20:25:04 +0200

    gradients working

--- my-portfolio/src/components/intro.js
+++ my-portfolio/src/components/intro.js
@@ -4,16 +4,18 @@ import styled from 'styled-components'
 
 const Box = styled.div`
 position:absolute;
-left:46.9%;
-top:35%;
-transform: translate(-50%,-50%)
-width:100%;
+left:50%;
+top:50%;
+transform: translate(-50%,-50%);
 display:flex;
-height:40%;
+width:50%;
+height:30%;
 background-repeat: no-repeat; 
 background-size: 100% 2px;
-background: linear-gradient( to right, white 50%, black 50%);
-
+background: linear-gradient( to right, white 50%, black 50%) bottom,
+linear-gradient( to right,white 50%, black 50%) top;
+border-left: 2px solid ${props =>props.theme.body};
+border-right: 2px solid ${props =>props.theme.text};
 // background: linear-gradient(
 //     to right,
 //     ${props => props.theme.body} 50%,

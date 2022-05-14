b5d1595e64c46a28f0572dcacdc187e51244a800
lifeKhomola <khomolavhutshilo5@gmail.com>
2022-05-14 05:08:16 +0200

    blogpage done

--- my-portfolio/src/components/BlogPage.js
+++ my-portfolio/src/components/BlogPage.js
@@ -10,15 +10,15 @@ import {Blogs} from '../data/BlogData'
 
 
 const MainContainer = styled.div`
-background-image:url(${img});
-background-size:cover;
-background-repeat:no-repeat;
+background-image: url(${img});
+background-size: cover;
+background-repeat: no-repeat;
 background-position:center;
 background-attachment:fixed;
 
 `
-const Container =styled.div`
-background-color:${props => `rgba(${props.theme.bodyRgba},0,8)`};
+const Container = styled.div`
+background-color: ${props => `rgba(${props.theme.bodyRgba},0.8)`};
 width:100%;
 height:auto;
 position:relative;

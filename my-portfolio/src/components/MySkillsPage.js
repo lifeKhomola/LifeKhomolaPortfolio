8e83d8c4baf429605316322b57041ccfe347d0ff
lifeKhomola <khomolavhutshilo5@gmail.com>
2022-05-16 12:30:38 +0200

    particles working

--- my-portfolio/src/components/MySkillsPage.js
+++ my-portfolio/src/components/MySkillsPage.js
@@ -2,11 +2,14 @@ import React from 'react'
 import {lightTheme} from './Themes'
 import styled,{ThemeProvider} from 'styled-components'
 import { Design,Developer } from './AllSvgs'
-
+import LogoComponent from '../subComponents/LogoComponent'
+import SocialIcons from '../subComponents/SocialIcons'
+import Home from '../subComponents/home'
+import ParticleComponent from '../subComponents/ParticlesComponent'
 
 const Box = styled.div`
 background-color: ${props => props.theme.body};
-width:100vw;
+width:100%;
 height:100vh;
 position:relative;
 display:flex;
@@ -20,13 +23,19 @@ color: ${props => props.theme.text};
 background-color: ${props => props.theme.body};
 padding: 2rem;
 width:30vw;
-height:60vh;
+height:;
 z-index:3;
 line-height:1.5;
+cursor:pointer;
 font-family:'Ubuntu Mono', monospace;
 display:flex;
 flex-direction: column;
 justify-content: space-between;
+
+&:hover{
+    color: ${props => props.theme.body};
+    background-color: ${props => props.theme.text};
+}
 
 `
 const Title = styled.h2`
@@ -35,26 +44,54 @@ justify-content: center;
 align-items: center;
 font-size: calc(1rem + 1 vw);
 
+${Main}:hover &{
+    &>*{
+        fill:${props => props.theme.body}
+    }
+}
+&>*:first-child{
+    margin-left: 1rem;
+}
 `
 const Description = styled.div`
-color: ${props => props.theme.theme.text};
+color: ${props => props.theme.text};
 font-size: calc(0.6em + 1vw);
 padding: 0.5rem 0;
+
+${Main}:hover &{
+   
+        color:${props => props.theme.body}
+    
+}
+strong{
+    margin-bottom: 1rem;
+    text-transform: uppercase;
+}
+ul,p{
+    margin-left:2rem;
+}
+
+
+
 `
 
 const MySkillsPage =()=>{
     return (
         <ThemeProvider theme={lightTheme}>
             <Box>
+            <ParticleComponent theme='light'/>
+                <LogoComponent  theme='light'/>
+                <SocialIcons  theme='light'/>
+                <Home/>
+                
                 <Main>
                        <Title>
-                           <Design width={40} height={40}/>
+                           <Design width={40} height={40}/> Backend Developer
                        </Title>
+                       <Description> I value business or brand for which i'm creating, thus i enjoy bringing new ideas to life.</Description>
                        <Description>
-                       I love to create design which speaks, Keep it clean, minimal and simple.
-                       </Description>
-                       {/* <Description>
-                           <strong>I like to Design</strong>
+                           
+                           <strong>Skills</strong>
                            <ul>
                                <li>
                                    Web Design
@@ -63,19 +100,31 @@ const MySkillsPage =()=>{
                                    Web Design
                                </li>   
                            </ul>
-                       </Description>  */}
+                       </Description>
+                       <Description>
+                           
+                           <strong>Tools</strong>
+                           <ul>
+                               <li>
+                                   Web Design
+                               </li>
+                               <li>
+                                   Web Design
+                               </li>   
+                           </ul>
+                       </Description>  
                 </Main>
                 <Main> 
                 <Title>
-                           <Developer width={40} height={40}/>
+                           <Developer width={40} height={40}/> Frontend Developer
                        </Title>
-{/*                 
+                
                 <Description>
                 I value business or brand for which i'm creating, thus i enjoy bringing new ideas to life.
 
                        </Description>
                        <Description>
-                           <strong>I like to Design</strong>
+                           <strong>Skills</strong>
                            <ul>
                                <li>
                                    Web Design
@@ -84,7 +133,19 @@ const MySkillsPage =()=>{
                                    Web Design
                                </li>   
                            </ul>
-                       </Description> */}
+                       </Description>
+                       <Description>
+                           
+                           <strong>Tools</strong>
+                           <ul>
+                               <li>
+                                   Web Design
+                               </li>
+                               <li>
+                                   Web Design
+                               </li>   
+                           </ul>
+                       </Description> 
                        </Main> 
             </Box>
         </ThemeProvider>

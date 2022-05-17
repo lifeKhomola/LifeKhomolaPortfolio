8514d6cacc0c8dd5ccf02f46bc7407fb3b6ff883
lifeKhomola <khomolavhutshilo5@gmail.com>
2022-05-17 05:49:26 +0200

    responsiveness

--- my-portfolio/src/components/Workpage.js
+++ my-portfolio/src/components/Workpage.js
@@ -14,18 +14,62 @@ background-color: ${props => props.theme.body};
 height:400vh;
 position:relative;
 overflow:hidden;
+
 `
 
 
-
-
 const Main = styled.div`
+@media only screen and (max-device-width: 480px) {
+    /* styles for mobile browsers smaller than 480px; (iPhone) */
+    width: 10rem;
+    margin-right: 4rem;
+    height: 40vh;
+ }
+ @media only screen and (max-width: 960px) {
+    /* styles for browsers larger than 960px; */
+    width: 10rem;
+    margin-right: 4rem;
+    height: 40vh;
+    left:calc(5rem + 5vw);
+}
+@media only screen and (min-width: 1440px) {
+    /* styles for browsers larger than 1440px; */
+}
+@media only screen and (min-width: 2000px) {
+    /* for sumo sized (mac) screens */
+}
+// @media only screen and (device-width: 768px) {
+//    /* default iPad screens */
+//    width: 10rem;
+//    margin-right: 4rem;
+//    height: 40vh;
+   
+// }
+/* different techniques for iPad screening */
+@media only screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:portrait) {
+  /* For portrait layouts only */
+}
+
+// @media only screen and (min-device-width: 481px) and (max-device-width: 1024px) and (orientation:landscape) {
+//   /* For landscape layouts only */
+//   width: 10rem;
+//     margin-right: 4rem;
+//     height: 40vh;
+// }
+// @media only screen and (min-device-width: 350px) and (max-device-width: 1024px) and (orientation:landscape) {
+//     /* For landscape layouts only */
+//     width: 10rem;
+//       margin-right: 4rem;
+//       height: 40vh;
+//   }
+
 position:fixed;
 top:12rem;
 left:calc(10rem + 15vw);
 height:40vh;
 display:flex;
 color:white;
+
 `
 
 
